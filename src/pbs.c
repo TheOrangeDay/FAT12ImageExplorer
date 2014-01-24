035a48c398f874a8109d25a55614aba5c76cbc1f
DanHoughton <daniel@danielhoughton.net>
2014-01-24 16:55:30 -0500

    Moved and wrote boot sector code
    Moved the pbs files, and wrote code to print the details of the boot
    sector

---
+++ src/pbs.c
@@ -0,0 +1,69 @@
+#include "pbs.h"
+#include "fatSupport.h"
+#include <memory.h>
+#include <stdio.h>
+/************************************************************************/
+/* Print boot sector command
+/* will be used to print the information about the current file system
+/*
+/*
+/* Written by: Daniel Houghton
+/************************************************************************/
+
+FATAttributes BOOT_SECTOR_ATTRIBUTES;
+
+/*struct FATAttributes 
+{
+unsigned short BytesPerSector;
+unsigned char SectorsPerCluster;
+unsigned char NumberOfFAT;
+unsigned char NumberOfReservedSectors;
+unsigned char NumberOfRootEntries;
+unsigned short TotalSectorCount;
+unsigned char SectorsPerFat;
+unsigned char SectorsPerTrack;
+unsigned char NumberOfHeads;
+unsigned char BootSignature;
+unsigned short VolumeID;
+char volumeLabel[32];
+char FileSystemType;
+};*/
+
+void readBootSector(void)
+{
+	unsigned char* bootSector;
+	int bytesRead;
+
+	bytesRead = read_sector(0, bootSector);
+
+	memcpy(BOOT_SECTOR_ATTRIBUTES.BytesPerSector, bootSector+11, 2);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.SectorsPerCluster, bootSector+13, 1);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.NumberOfReservedSectors, bootSector+14, 2);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.NumberOfFAT, bootSector+16, 1);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.NumberOfRootEntries, bootSector+17, 2);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.TotalSectorCount, bootSector+19, 2);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.SectorsPerFat, bootSector+22, 2);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.SectorsPerTrack, bootSector+24, 2);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.NumberOfHeads, bootSector+26, 2);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.BootSignature, bootSector+38, 1);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.VolumeID, bootSector+39, 4);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.volumeLabel, bootSector+43, 11);
+	memcpy(BOOT_SECTOR_ATTRIBUTES.FileSystemType, bootSector+54, 8);
+}
+
+void printBootSector(void)
+{
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.BytesPerSector);
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.SectorsPerCluster);
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.NumberOfReservedSectors);
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.NumberOfFAT);
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.NumberOfRootEntries);
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.TotalSectorCount);
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.SectorsPerFat);
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.SectorsPerTrack);
+	printf("%d", BOOT_SECTOR_ATTRIBUTES.NumberOfHeads);
+	printf("0x%x", BOOT_SECTOR_ATTRIBUTES.BootSignature);
+	printf("0x%x", BOOT_SECTOR_ATTRIBUTES.VolumeID);
+	printf("%s", BOOT_SECTOR_ATTRIBUTES.volumeLabel);
+	printf("%s", BOOT_SECTOR_ATTRIBUTES.FileSystemType);
+}
---
+++ src/pbs.h
@@ -0,0 +1,21 @@
+#ifndef PBS_H
+#define PBS_H
+
+struct FATAttributes 
+{
+	unsigned char BytesPerSector[2];
+	unsigned char SectorsPerCluster[1];
+	unsigned char NumberOfFAT[1];
+	unsigned char NumberOfReservedSectors[2];
+	unsigned char NumberOfRootEntries[2];
+	unsigned char TotalSectorCount[2];
+	unsigned char SectorsPerFat[2];
+	unsigned char SectorsPerTrack[2];
+	unsigned char NumberOfHeads[2];
+	unsigned char BootSignature[1];
+	unsigned char VolumeID[4];
+	unsigned char volumeLabel[11];
+	unsigned char FileSystemType[8];
+};
+
+#endif
